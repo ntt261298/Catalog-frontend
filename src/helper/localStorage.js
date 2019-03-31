33a634b727a797c47101106d237ee93fd93722cb
truongnt <ntt261298@gmail.com>
2019-03-31 15:54:20 +0700

    Add localStorage

---
+++ src/helper/localStorage.js
@@ -0,0 +1,29 @@
+export const loadState= () => {
+    try {
+      const seriallizedState = localStorage.getItem('state');
+      if(seriallizedState === null) {
+        return undefined;
+      }
+      return JSON.parse(seriallizedState);
+    } catch (err) {
+      return undefined;
+    }
+  }
+  
+export const saveState = (state) => {
+    try {
+      const seriallizedState = JSON.stringify(state);
+      localStorage.setItem('state', seriallizedState);
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+export const removeState = (state) => {
+    try {
+      const seriallizedState = JSON.stringify(state);
+      localStorage.removeItem('state', seriallizedState);
+    } catch (err) {
+      console.error(err)
+    }
+  }
